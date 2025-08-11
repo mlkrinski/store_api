b9bd432cd1e5297496f47e0851149434880000db
mlkrinski <marcelokrinski@gmail.com>
2025-08-11 20:04:04 -0300

    feat: add .bat file

--- run.bat
+++ run.bat
@@ -4,6 +4,7 @@ echo  Dev Tools
 echo ==============================
 echo 1 - Run API
 echo 2 - Install pre-commit
+echo 2 - Pytest
 echo ==============================
 set /p choice="Escolha uma opcao: "
 
@@ -11,6 +12,8 @@ if "%choice%"=="1" (
     uvicorn store.main:app --reload
 ) else if "%choice%"=="2" (
     poetry run pre-commit install
+) else if "%choice%" == "3" (
+    poetry run pytest
 ) else (
     echo Opcao invalida.
 )
