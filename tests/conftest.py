a5dc55a592b1ac0cceabb1b94dcb9716e1b82f8a
mlkrinski <marcelokrinski@gmail.com>
2025-08-12 19:54:05 -0300

    feat: mongodb docker and conftest

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+# guardar as fixtures
+
+import asyncio
+from store.db.mongo import db_client
+import pytest
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture
+def mongo_client():
+    return db_client.get()
+
+
+@pytest.fixture(autouse=True)
+async def clear_collections(mongo_client):
+    yield
+    collections_names = await mongo_client.get_database().list_collection_names()
+    for collection_name in collections_names:
+        if collection_name.startswith("system"):
+            continue
+
+        await mongo_client.get_database()[collection_name].delete_many({})
--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -1,21 +1,20 @@
 from uuid import UUID
-
 from pydantic import ValidationError
 import pytest
 from store.schemas.product import ProductIn
 from tests.factories import product_data
 
 
-def test_scheas_return_success():
+def test_schemas_return_success():
     data = product_data()
     product = ProductIn.model_validate(data)
 
-    assert product.name == "Iphone 14 pro Max"
+    assert product.name == "Iphone 14 Pro Max"
     assert isinstance(product.id, UUID)
 
 
-def test_scheas_return_raise():
-    data = {"name": "Iphone 14 pro Max", "quantity": 10, "price": 8.500}
+def test_schemas_return_raise():
+    data = {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.500}
 
     with pytest.raises(ValidationError) as err:
         ProductIn.model_validate(data)
@@ -24,6 +23,6 @@ def test_scheas_return_raise():
         "type": "missing",
         "loc": ("status",),
         "msg": "Field required",
-        "input": {"name": "Iphone 14 pro Max", "quantity": 10, "price": 8.5},
+        "input": {"name": "Iphone 14 Pro Max", "quantity": 10, "price": 8.5},
         "url": "https://errors.pydantic.dev/2.11/v/missing",
     }
